428a47745714a0475eb50d90cbc036f8672d4a37
michaelolchang <michaelchang.mhc@gmail.com>
2020-04-21 16:10:00 -0400

    Incorporated section classes to organize code

--- ReportGraphic.java
+++ ReportGraphic.java
@@ -2,294 +2,127 @@ import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.event.*;
 import java.awt.*;
+import java.util.Date;
+import javax.swing.BorderFactory;
+import javax.swing.border.*;
 
 class ReportGraphic {
     private JFrame reportFrame;
     private JPanel reportPanel;
+    private JPanel buttonPanel;
+    private JButton returnToMain;
+    private JButton saveToPdf;
 
-    // Main Table 
-    // ----------------------------------------------------------------------
-    private JTable reportTable;
-    private UneditModel reportModel;
-    private JTableHeader header;
-    private JScrollPane tableScroll;
-    // ----------------------------------------------------------------------
-
-    // Income/Spending Total
-    // ----------------------------------------------------------------------
-    private JTable totalTable;
-    private UneditModel totalModel;
-    private JScrollPane totalScroll;
-    // ----------------------------------------------------------------------
-
-    // Net Total
-    // ----------------------------------------------------------------------
-    private JTable netTable;
-    private JScrollPane netScroll;
-    // ----------------------------------------------------------------------
-
-
-    JButton b1;
-    JButton b2;
-
-
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         ReportGraphic report = new ReportGraphic();
         report.activate();
     }
 
     public void activate() {
-        reportFrame = new JFrame("Report");
+       
 
-        Color defaultColor = new Color(238,238,238);
-
-        // Panel Formatting
-        // ----------------------------------------------------------------------
-        reportPanel = new JPanel();
-        reportPanel.setLayout(new GridBagLayout());
-        reportPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        // ----------------------------------------------------------------------
-
-
-        // Table Formatting 
-        // ----------------------------------------------------------------------
+        Date date = new Date();
         Object[] colNames = {"Date", "Name", "Income", "Spendings"};
         Object[][] data = {
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,"",10,11},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8},
-            {5,6,7,8}};
-
-        reportTable = new JTable();
-        header = reportTable.getTableHeader();
-        tableScroll = new JScrollPane(reportTable);
-        DefaultTableModel reportModel = new DefaultTableModel(data, colNames) {
-            @Override
-            public boolean isCellEditable(int row, int col) {
-                return false;
-            }
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
+            {date,"Test",3,4},
         };
 
-        reportTable.setModel(reportModel);
+        reportFrame = new JFrame("Report");
+        reportPanel = new JPanel();
+        buttonPanel = new JPanel();
 
-        header.setDefaultRenderer(new HeaderRenderer(reportTable));
-        header.setReorderingAllowed(false);
-        header.setResizingAllowed(false);
+        reportPanel.setLayout(new GridBagLayout());
+        buttonPanel.setLayout(new GridBagLayout());
 
+        reportPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
-        reportTable.getColumnModel().getColumn(0).setPreferredWidth(100);
-        reportTable.getColumnModel().getColumn(1).setPreferredWidth(300);
-        reportTable.getColumnModel().getColumn(2).setPreferredWidth(90);
-        reportTable.getColumnModel().getColumn(3).setPreferredWidth(90);
-        
-        reportTable.setShowHorizontalLines(false);
-        
-        if(reportTable.getPreferredSize().height <= 240) {
-            reportTable.setPreferredScrollableViewportSize(reportTable.getPreferredSize());
-        } else {
-            reportTable.setPreferredScrollableViewportSize(
-                    new Dimension(reportTable.getPreferredSize().width, 240));
-        }
-        // ----------------------------------------------------------------------
+        MainReportTableSection mainSection = new MainReportTableSection(data, colNames);
+        TotalSection totalSection = new TotalSection();
+        NetTotalSection netSection = new NetTotalSection();
 
-        // Income/Spending Total Section
-        // ----------------------------------------------------------------------
-        Object[] totalName = {"Total", "Income Total", "Spending total"};
-        Object[][] totalData = {{"Income/Spending Total: ", 1000, 200}};
+        JScrollPane mainPane = mainSection.createMainTable();
+        JScrollPane totalPane = totalSection.createTotalTable();
+        JScrollPane netTotalPane = netSection.createNetTotalTable();
 
-        totalTable = new JTable();
-        totalModel = new UneditModel(totalData, totalName);
-        totalTable.setRowSelectionAllowed(false);
-        totalTable.setTableHeader(null);
+        JLabel space = new JLabel(" ");
+        JLabel space2 = new JLabel(" ");
+        JLabel buttonSpace = new JLabel("<html><br/><br/><br/></html>");
 
-        totalTable.setModel(totalModel);
-        CellRenderer totalRenderer = new CellRenderer();
-        totalRenderer.setHorizontalAlignment(JLabel.RIGHT);
-        totalTable.getColumnModel().getColumn(0).setCellRenderer(totalRenderer);
+        returnToMain = new JButton("Return to the Main Menu");
+        saveToPdf = new JButton("Save to PDF");
 
-        totalTable.getColumnModel().getColumn(0).setPreferredWidth(400);
-        totalTable.getColumnModel().getColumn(1).setPreferredWidth(90);
-        totalTable.getColumnModel().getColumn(2).setPreferredWidth(90);
+        Border defaultButtonBorder = returnToMain.getBorder();
 
-        totalTable.setBackground(defaultColor);
+        saveToPdf.setEnabled(false);
 
-        totalTable.setPreferredScrollableViewportSize(totalTable.getPreferredSize());
+        buttonPanel.add(saveToPdf, reportGbc(0,0));
+        buttonPanel.add(buttonSpace, reportGbc(1,0));
+        buttonPanel.add(returnToMain, reportGbc(2,0));
 
-        totalScroll = new JScrollPane(totalTable);
-        // ----------------------------------------------------------------------
-        
-        // Net Income Section 
-        // ----------------------------------------------------------------------
-        Object[] netName = {"Net Total", "Total"};
-        Object[][] netTotal = {{"Net Total: ", 10000}};
+        reportPanel.add(mainPane, reportGbc(0,0));
+        reportPanel.add(totalPane, reportGbc(0,1));
+        reportPanel.add(space, reportGbc(0,2));
+        reportPanel.add(netTotalPane, reportGbc(0,3));
+        reportPanel.add(space2, reportGbc(0,4));
+        reportPanel.add(buttonPanel, reportGbc(0,5));
 
-        netTable = new JTable(netTotal, netName);
-        
-        netTable.setTableHeader(null);
-        netTable.setShowVerticalLines(false);
-        netTable.setPreferredScrollableViewportSize(netTable.getPreferredSize());
-
-        CellRenderer netRenderer = new CellRenderer();
-        netTable.getColumnModel().getColumn(0).setCellRenderer(netRenderer);
-
-        netScroll = new JScrollPane(netTable);
-        // ----------------------------------------------------------------------
-
-
-        b1 = new JButton("before");
-        b2 = new JButton("after");
-        JLabel test = new JLabel(" ");
-
-        b1.addActionListener(new B1Listener());
-        
-        reportPanel.add(tableScroll, reportGbc(0,0));
-        reportPanel.add(totalScroll, reportGbc(0,1));
-        reportPanel.add(test, reportGbc(0,2));
-        reportPanel.add(netScroll, reportGbc(0,3));
-        reportPanel.add(b1, reportGbc(0,4));
-        reportPanel.add(b2, reportGbc(0,5));
-        
-        reportFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         reportFrame.getContentPane().add(reportPanel);
+        reportFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         reportFrame.pack();
         reportFrame.setVisible(true);
         reportFrame.setResizable(false);
 
-        b2.addActionListener(new B2Listener());
-        
+        Border defaultPaneBorder = totalPane.getBorder();
+
+        int scrollBarWidth = mainPane.getVerticalScrollBar().getSize().width;
+        netTotalPane.setBorder(BorderFactory.createCompoundBorder(
+                new EmptyBorder(0, 0, 0, scrollBarWidth),
+                defaultPaneBorder));
     }
 
-    // GridBagConstraint
-    // ----------------------------------------------------------------------
     private GridBagConstraints reportGbc(int x, int y) {
         GridBagConstraints gbc = new GridBagConstraints();
-         
+
         gbc.gridx = x;
         gbc.gridy = y;
-        
-        if(y == 0 || y == 1) {
+
+        if(y == 1) {
             gbc.anchor = GridBagConstraints.WEST;
             gbc.weightx = 1;
-        }else if(y == 3) {
+        } else if(y == 3) {
             gbc.anchor = GridBagConstraints.EAST;
             gbc.weightx = 1;
         }
-            
+
         return gbc;
     }
-    // ----------------------------------------------------------------------
-
-
-// HEADER RENDERING
-    // Header renderer (Aligning left)
-    // ----------------------------------------------------------------------
-    private class HeaderRenderer implements TableCellRenderer {
-        DefaultTableCellRenderer renderer;
-
-        public HeaderRenderer(JTable table) {
-            renderer = (DefaultTableCellRenderer) table.getTableHeader().getDefaultRenderer();
-            renderer.setHorizontalAlignment(JLabel.LEFT);
-        }
-
-        @Override
-        public Component getTableCellRendererComponent( 
-                JTable table, Object value, boolean isSelected,
-                boolean hasFocus, int row, int col) {
-            return renderer.getTableCellRendererComponent(
-                    table, value, 
-                    isSelected, hasFocus, 
-                    row, col);
-        }
-    }
-    // ----------------------------------------------------------------------
-    
-    // Cell renderer (Bold)
-    // ----------------------------------------------------------------------
-    public class CellRenderer extends DefaultTableCellRenderer {
-        public Component getTableCellRendererComponent(
-                JTable table, Object value,
-                boolean isSelected, boolean hasFocus,
-                int row, int col) {
-            Component renderer = super.getTableCellRendererComponent(
-                    table, value,
-                    isSelected, hasFocus,
-                    row, col);
-
-            if(value == "Income/Spending Total: ") {
-                renderer.setFont(renderer.getFont().deriveFont(Font.BOLD));
-            } else if(value == "Net Total: ") {
-                renderer.setFont(renderer.getFont().deriveFont(Font.BOLD));
-            }
-
-            return renderer;
-        }
-        
-    }
-    // ----------------------------------------------------------------------
-
-
-    // Table Model (No editting)
-    // ----------------------------------------------------------------------
-    private class UneditModel extends DefaultTableModel {
-
-        public UneditModel(Object[][] data, Object[] col) {
-            super(data, col);
-        }
-            
-        @Override
-        public boolean isCellEditable(int row, int col) {
-                return false;
-        }
-    }
-    // ----------------------------------------------------------------------
-
-    public class B1Listener implements ActionListener {
-        public void actionPerformed(ActionEvent ev) {
-            System.out.println("------------------------------------------------");
-            System.out.println(tableScroll.getPreferredSize());
-            System.out.println(reportTable.getPreferredSize());
-            System.out.println(reportPanel.getPreferredSize());
-            System.out.println(reportFrame.getPreferredSize());
-            System.out.println("------------------------------------------------");
-            System.out.println(reportPanel.getSize());
-            System.out.println(reportFrame.getSize());
-            
-        }
-    }
-
-    public class B2Listener implements ActionListener {
-        public void actionPerformed(ActionEvent ev) {
-        }
-                    
-    }
-
 }
